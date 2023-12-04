27f81678be76251b93c61db5803004d0b65d6799
bben-aou <windoxman@gmail.com>
2023-12-04 17:51:30 +0100

    fix : adjust responsive for md and xs screens in about page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -21,6 +21,8 @@ export default function About() {
     const { openMenu } = useContext(AppContext);
     const isMdScreen = useMediaQuery({ minWidth: 768 });
     const isLgScreen = useMediaQuery({ minWidth: 1200 });
+    const isXsScreen = useMediaQuery({ maxWidth: 375 });
+
     // const [showSocialMedia, setShowSocialMedia] = useState(true);
 
     // useEffect(() => {
@@ -37,27 +39,32 @@ export default function About() {
         <>
             <Header />
             <div className={!openMenu ? `main-about ` : `hidden`}>
-                <div className={`w-full h-[20%] lg:w-[35%] lg:h-full  `}>
-                    <div className=" w-full lg:h-3/5 2xl:h-1/2 flex flex-col justify-end items-center ">
+                <div className={`w-full h-[20%] lg:w-[35%] md:w-full md:h-[20%]   lg:h-full  `}>
+                    <div className=" w-full h-full md:h-full lg:h-3/5 2xl:h-1/2 flex flex-col justify-end lg:justify-end items-center  ">
                         <img src={profile} alt="profile" className=" custom-class shadow-profile  shadow-[#445964b0] animate-profile " />
-                        {(isMdScreen || isLgScreen) && <SocialMedia />}
+                        {(isLgScreen) && <SocialMedia />}
                     </div>
-                    {(isMdScreen || isLgScreen) && <Quote
+                    {( isLgScreen) && <Quote
                         quoteContent={"“Learning is the only thing that the mind never gets tired of, is never afraid of and never regrets”"}
                         author={"Leonardo Da Vinci"}
                     />}
                 </div>
-                <div className='w-full  h-[80%] lg:h-full  flex flex-col lg:justify-center  '>
-                    <div className='w-full h-[70%]  px-[50px] 2xl:px-[70px] flex flex-col items-center 2xl:justify-evenly lg:pt-[45px] '>
-                        <div className='w-full  font-quote font-bold text-[3.5rem] 2xl:text-[5rem] flex items-end justify-center text-dark-900 scale-up-center'>
+                <div className='w-full h-[80%] md:w-full md:h-[80%] lg:h-full   flex flex-col lg:justify-center  '>
+                    <div className={`w-full  h-full md:h-full lg:h-[70%]  px-[50px] 2xl:px-[70px] flex flex-col md:justify-center lg:justify-normal items-center  2xl:justify-evenly lg:pt-[45px]`}>
+                        <div className={`w-full font-quote font-bold text-[3.5rem] 2xl:text-[5rem] flex items-end justify-center text-dark-900 scale-up-center`}>
                             I'm Bilal
                         </div>
-                        <p className='w-full text-center font-roboto text-[1rem] lg:text-[1.2rem]  text-dark-900 leading-16 2xl:leading-10 scale-up-center'>
+                        <p className={`w-full ${isXsScreen ? `leading-6 text-justify` : `leading-8 lg:leading-9 2xl:leading-10 text-center `}  font-roboto text-[1rem] lg:text-[1.2rem]  text-dark-900  scale-up-center`}>
                             A passionate software engineer with over three years of dedicated study in the field. I thrive in front-end development but constantly seek opportunities to challenge myself with new technologies. When I'm not coding, you'll find me strumming the guitar, exploring new destinations, or camping with friends. With a thirst for knowledge and a drive for growth, I'm ready to take on new challenges and contribute to the ever-evolving world of IT.
                         </p>
-                        <button className=' p-2 lg:p-0 w-[150px]  lg:h-[50px] bg-[#3E2938] rounded-md mt-[1rem] font-roboto hover:bg-[#414B59] transition duration-700 ease-in-out hover:scale-105 scale-up-center' onClick={handleBtnClick}>
+                        <button className=' p-2 lg:p-0 w-[150px]  lg:h-[50px] bg-[#3E2938] rounded-md mt-4 md:mt-6 font-roboto hover:bg-[#414B59] transition duration-700 ease-in-out hover:scale-105 scale-up-center' onClick={handleBtnClick}>
                             Contact Me
                         </button>
+                        {( isMdScreen && !isLgScreen) && <Quote
+                        quoteContent={"“Learning is the only thing that the mind never gets tired of, is never afraid of and never regrets”"}
+                        author={"Leonardo Da Vinci"}
+                    />}
+                        {(!isMdScreen && ! isLgScreen && !isXsScreen) && <SocialMedia />}
                     </div>
                 </div>
 
