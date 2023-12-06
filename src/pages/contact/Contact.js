b463ac3516205def6d90650124fb8b4f74a9bde6
bben-aou <windoxman@gmail.com>
2023-12-06 17:39:10 +0100

    chore(contact page) : refactor the styling classes

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -22,22 +22,25 @@ export default function Contact() {
     return (
         <>
             <Header />
-            <div class={!openMenu ? `w-full h-[92vh] lg:h-[88vh] lg:px-[50px] flex ` : `hidden`}>
-                <div className={` h-full ${(isMdScreen || isLgScreen) ? `w-[60%]` : `w-full`} flex items-center justify-center `}>
+            <div class={!openMenu ? `container ` : `hidden`}>
+                <div className={`${(isMdScreen || isLgScreen) ? `w-[60%]` : `w-full`} wrapper`}>
                     <div className={(isMdScreen || isLgScreen) ? `w-[90%] h-[90%] rounded-lg  flex flex-col justify-center` : `w-full h-full  rounded-lg `}>
-                        <div className={`w-full  h-[25%] md:h-[20%] 2xl:h-[30%]   ${(isMdScreen || isLgScreen) ? `justify-center ` : `justify-end`} justify-center flex flex-col items-center `}>
-                            <h1 className=' text-[1.4rem] lg:text-[2rem] 2xl:text-[3rem] 2xl:tracking-wide font-semibold font-roboto text-dark-900 tracking-[0.5px] dark:text-light-900 slide-top'>
+                        <div className={`info-section ${(isMdScreen || isLgScreen) ? `justify-center ` : `justify-end`} `}>
+                            <h1 className='contact-title'>
                                 Contact
                             </h1>
-                            <p className='lg:text-right text-center text-dark-800 text-[0.8rem] lg:text-[1rem] font-normal font-roboto lg:mt-2 2xl:mt-4  dark:text-light-800 slide-right'>
-                                Get in touch or shoot me an email directly on <span className='text-dark-900 dark:text-light-800 font-semibold transition duration-700 ease-in-out email-animation'> bilal.benaouad@ump.ac.com </span>
+                            <p className='paragraph-styling'>
+                                Get in touch or shoot me an email directly on
+                                <span className='email-styling'>
+                                    bilal.benaouad@ump.ac.com
+                                </span>
                             </p>
                         </div>
-                        <div className='w-full h-[75%] md:h-[50%] lg:h-[60%]  2xl:h-[70%] 2xl:justify-start flex flex-col md:justify-center px-[50px] '>
+                        <div className='form-container'>
 
-                            < EmailForm/>
+                            < EmailForm />
                             {
-                               showSocialMedia &&
+                                showSocialMedia &&
                                 <div className='w-full h-[20%] flex items-end'>
                                     <SocialMedia />
                                 </div>
