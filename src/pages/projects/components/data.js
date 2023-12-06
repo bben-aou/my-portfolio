0fa9be0c9aef23491a75eb8573a386e078740655
bben-aou <windoxman@gmail.com>
2023-12-06 20:30:35 +0100

    fix(project page) : adjust data file

--- src/pages/projects/components/data.js
+++ src/pages/projects/components/data.js
@@ -3,64 +3,18 @@ import icons from "../../../assets/icons/icons";
 
 const projectsData = [
     {
-      projectTitle: "libft",
-      projectDescription: "Create a versatile C library for essential functions in C projects.",
-      projectImgCard: icons.libftCardImg,
-      technologies : "lowLevel",
-      githubPath : "https://github.com/bben-aou/libft",
+        projectTitle: "ft_transcendence",
+        projectDescription: "Web project, about a real-time ping pong game and more ...",
+        projectImgCard: icons.pingCardImg,
+        technologies : "Web",
+        githubPath : "https://github.com/bben-aou/ft_transcendence",
     },
     {
-      projectTitle: "get_next_line",
-      projectDescription: "Create a newline-ending line reader from a file descriptor.",
-      projectImgCard: icons.getLineCardImg,
-      technologies : "lowLevel",
-      githubPath : "https://github.com/bben-aou/get_next_line",
-
-    },
-    {
-        projectTitle: "ft_printf",
-        projectDescription: "Recreate the printf function with a personalized approach.",
-        projectImgCard: icons.printfCardImg,
-        technologies : "lowLevel",
-        githubPath : "https://github.com/bben-aou/ft_printf",
-
-    },
-    {
-        projectTitle: "pipex",
-        projectDescription: "Recreate the way two commands are piped together by | in the shell.",
-        projectImgCard: icons.pipexCardImg,
-        technologies : "lowLevel",
-        githubPath : "https://github.com/bben-aou/pipex",
-
-    },
-    {
-        projectTitle: "push-swap",
-        projectDescription: "Algorithmic problem aims to find the best sort algorithm to solve it.",
-        projectImgCard: icons.pushSwapCardImg,
-        technologies : "lowLevel",
-        githubPath : "https://github.com/bben-aou/push-swap",
-
-    },
-    {
-        projectTitle: "philosophers",
-        projectDescription: "Solving Dining philosophers problem using the threads.",
-        projectImgCard: icons.philosopherCardImg,
-        technologies : "lowLevel",
-        githubPath : "https://github.com/bben-aou/philosopher",
-    },
-    {
-        projectTitle: "cub3d",
-        projectDescription: "Using graphic tools to make a raycasting game like Wolfenstein.",
-        projectImgCard: icons.cub3dCardImg,
-        technologies : "lowLevel",
-        githubPath : "https://github.com/bben-aou/Cub3D",
-    },
-    {
-        projectTitle: "ft_irc",
-        projectDescription: "Implement Internet Relay Chat Server based on RFC 2812.",
-        projectImgCard: icons.ircCardImg,
-        technologies : "C++",
-        githubPath : "https://github.com/bben-aou/ft_irc",
+        projectTitle: "University Website",
+        projectDescription: "one of the first websites i created when i started web development. ",
+        projectImgCard: icons.univCardImg,
+        technologies : "Basic",
+        githubPath : "https://bben-aou.github.io/myprojects/university/",
     },
     {
         projectTitle: "inception",
@@ -77,18 +31,64 @@ const projectsData = [
         githubPath : "https://github.com/bben-aou/CPP_Modules-",
     },
     {
-        projectTitle: "ft_transcendence",
-        projectDescription: "Web project, about a real-time ping pong game and more ...",
-        projectImgCard: icons.pingCardImg,
-        technologies : "Web",
-        githubPath : "https://github.com/bben-aou/ft_transcendence",
+        projectTitle: "ft_irc",
+        projectDescription: "Implement Internet Relay Chat Server based on RFC 2812.",
+        projectImgCard: icons.ircCardImg,
+        technologies : "C++",
+        githubPath : "https://github.com/bben-aou/ft_irc",
     },
     {
-        projectTitle: "University Website",
-        projectDescription: "one of the first websites i created when i started web development. ",
-        projectImgCard: icons.univCardImg,
-        technologies : "Basic",
-        githubPath : "https://bben-aou.github.io/myprojects/university/",
+        projectTitle: "cub3d",
+        projectDescription: "Using graphic tools to make a raycasting game like Wolfenstein.",
+        projectImgCard: icons.cub3dCardImg,
+        technologies : "lowLevel",
+        githubPath : "https://github.com/bben-aou/Cub3D",
     },
+    {
+        projectTitle: "philosophers",
+        projectDescription: "Solving Dining philosophers problem using the threads.",
+        projectImgCard: icons.philosopherCardImg,
+        technologies : "lowLevel",
+        githubPath : "https://github.com/bben-aou/philosopher",
+    },
+    {
+        projectTitle: "libft",
+        projectDescription: "Create a versatile C library for essential functions in C projects.",
+        projectImgCard: icons.libftCardImg,
+        technologies : "lowLevel",
+        githubPath : "https://github.com/bben-aou/libft",
+      },
+      {
+        projectTitle: "get_next_line",
+        projectDescription: "Create a newline-ending line reader from a file descriptor.",
+        projectImgCard: icons.getLineCardImg,
+        technologies : "lowLevel",
+        githubPath : "https://github.com/bben-aou/get_next_line",
+  
+      },
+      {
+          projectTitle: "ft_printf",
+          projectDescription: "Recreate the printf function with a personalized approach.",
+          projectImgCard: icons.printfCardImg,
+          technologies : "lowLevel",
+          githubPath : "https://github.com/bben-aou/ft_printf",
+  
+      },
+      {
+          projectTitle: "pipex",
+          projectDescription: "Recreate the way two commands are piped together by | in the shell.",
+          projectImgCard: icons.pipexCardImg,
+          technologies : "lowLevel",
+          githubPath : "https://github.com/bben-aou/pipex",
+  
+      },
+      {
+          projectTitle: "push-swap",
+          projectDescription: "Algorithmic problem aims to find the best sort algorithm to solve it.",
+          projectImgCard: icons.pushSwapCardImg,
+          technologies : "lowLevel",
+          githubPath : "https://github.com/bben-aou/push-swap",
+  
+      },
   ];
 export default projectsData;
